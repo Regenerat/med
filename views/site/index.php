4b49555966d00962485dd2a8ee2dd5b761290083
Megira <regenerat.55@mail.ru>
2024-02-28 01:32:18 +0300

    Отредактировал раздел "О нас"

--- views/site/index.php
+++ views/site/index.php
@@ -10,36 +10,45 @@ $this->title = 'My Yii Application';
         <h1 class="display-4">Медицинская клиника Мед-Плюс</h1>
     </div>
 
-    <div class="border border-primary rounded bg-info bg-gradient col-lg-12" style="height: 300px;">
+    <div class="br-block-green bg-gradient col-lg-12">
         <div class="container">
-            <div class="row mt-4 ms-2">
-                <div class="col-3">
-                    <div class="border border-primary rounded" style="height: 250px; width: 250px;">
-                        <img src="web\img\1.jpg" class="img-fluid rounded">
-                    </div>
+            <div class="row my-4 mx-2">
+                <div class="col-sm-3">              
+                    <img src="web\img\1.png" class="img-fluid rounded">             
                 </div>
-                <div class="col-5">
-                    <p class="lead d-flex justify-content-left fw-semibold">Веницианский хирург</p>
-                    <p class="fs-6 d-flex justify-content-left">„Сейчас распилю! ННЫАААА!!!“</p>
+                <div class="col-sm-9">
+                    <p class="lead d-flex justify-content-left fw-semibold">От первого лица</p>
+                    <p class=" d-flex justify-content-left">Холдинг «Мед-Плюс» - одна из крупнейших сетей многопрофильных медицинских центров для взрослых и детей. Холдинг входит в топ частных клиник по оценкам Forbes, РБК, Vademecum и BusinesStat.</p>
+                    <p class=" d-flex justify-content-left">Мы ведём свою историю с 2002 года. Сейчас у нас работают 29 многопрофильных клиник в Москве, Подмосковье, Санкт-Петербурге, Иванове и Рязани, которые открыты для вас ежедневно, включая выходные и праздничные дни.</p>
+                    <p class="lead d-flex justify-content-end fw-semibold">Николай Вениаминович Смыслов</p>
+                    <p class=" d-flex justify-content-end">генеральный директор группы компаний «Мед-Плюс»</p>
                 </div>               
             </div> 
         </div>            
     </div>
 
-    <div class="col-lg-12" style="height: 400px;">
+    <div class="col-sm-12">
         <div class="container">
-            <div class="row mt-5">
-                <div class="col-7 border border-primary rounded bg-info bg-gradient" style="height: 400px;">
-                    <div class="">
-                        
-                    </div>
+            <div class="row">
+                <div class="col-sm-7 br-block-grey mt-5 px-4 py-4">
+                    <p class="lead d-flex justify-content-left fw-semibold">Команда «Мед-Плюс»</p>
+                    <p class=" d-flex justify-content-left">В наших центрах работают опытные специалисты, которые с вниманием относятся к каждому пациенту. Это профессионалы, которые станут вашими партнёрами в сохранении здоровья.</p>
+                    <p class=" d-flex justify-content-left">Наша команда – более 3000 врачей различных специальностей. Среди них 197 кандидатов медицинских наук, 29 докторов медицинских наук, 8 профессоров, 2 академика РАМН, 7 доцентов и старших научных сотрудников, Заслуженный врач России.</p>
+                    <p class=" d-flex justify-content-left">В наших медицинских центрах работают врачи как самых востребованных медицинских специальностей: терапевты, кардиологи, урологи, гинекологи, офтальмологи, оториноларингологи, хирурги и другие, так и редких: генетики, репродуктологи, эмбриологи, гепатологи и другие.</p>
+                    <p class=" d-flex justify-content-left">В детских отделениях ведут приём доктора, специализирующиеся на лечении детей с первых дней жизни до совершеннолетия.</p>
                 </div>
-                <div class="col-5">
-                    <div class="border border-primary rounded bg-info bg-gradient" style="height: 100px;">
+                <div class="col-sm-5 mt-5 g-0 g-sm-5">
+                    <div class="br-block-green px-4 py-4">
+                        <p class="lead d-flex justify-content-left fw-semibold">> 3500</p>
+                        <p class=" d-flex justify-content-left">медицинских специалистов</p>
                     </div>
-                    <div class="border border-primary rounded bg-info bg-gradient mt-5" style="height: 100px;">
+                    <div class="br-block-green mt-5 px-4 py-4">
+                        <p class="lead d-flex justify-content-left fw-semibold">> 3000</p>
+                        <p class=" d-flex justify-content-left">врачей, в том числе редких специальностей</p>
                     </div>
-                    <div class="border border-primary rounded bg-info bg-gradient mt-5" style="height: 100px;">
+                    <div class="br-block-green mt-5 px-4 py-4">
+                        <p class="lead d-flex justify-content-left fw-semibold">38% врачей имеют ученые степени</p>
+                        <p class=" d-flex justify-content-left">доктора и кандидаты наук, научные сотрудники, доценты и профессора, врачи высшей, I и II категории, заслуженные врачи РФ</p>
                     </div> 
                 </div>               
             </div> 
